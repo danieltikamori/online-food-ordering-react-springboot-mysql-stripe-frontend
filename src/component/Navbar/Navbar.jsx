9bac45a2f62a2c54be8dc523a2d53224b4c6e7ff
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-08-12 20:55:58 +0900

    Fix: color to be equal Japan flag red

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -8,9 +8,9 @@ import "./Navbar.css"
 
 export default function Navbar() {
   return (
-    <div className="px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
+    <div className="px-5 z-50 py-[.8rem] bg-[#BC002D] lg:px-20 flex justify-between">
       <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
-        <li className="logo font-semibold text-gray-300 text-2xl">Tika Food</li>
+        <li className="logo font-semibold text-gray-300 text-2xl">Tika Foods</li>
       </div>
 
       <div className="flex items-center space-x-2 lg:space-x-10">
@@ -28,7 +28,7 @@ export default function Navbar() {
         <div className="">
 
         <IconButton>
-          <Badge color="primary" badgeContent={3}>
+          <Badge color="secondary" badgeContent={3}>
             <ShoppingCartIcon sx={{ fontSize: "1.5rem" }}/>
           </Badge>
 
