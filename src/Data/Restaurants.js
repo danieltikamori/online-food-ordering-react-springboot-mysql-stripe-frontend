79428994ffba0b2cc585808dbb05ea1ee1e55d87
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-08-12 20:55:00 +0900

    Add Restaurants samples
    Samples of restaurants

---
+++ src/Data/Restaurants.js
@@ -0,0 +1,90 @@
+export const Restaurants = [
+    {
+        "imageUrl": "https://images.pexels.com/photos/3714786/pexels-photo-3714786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "name": "McDonalds",
+        "rating": 8.1,
+        "city": "Mount Pleasant",
+        "description": "The world's largest and most popular fast-food brands, serving a variety of items such as hamburgers, fries, chicken products, breakfast items, and more."
+    },
+    {
+        "imageUrl": "https://images.pexels.com/photos/14534749/pexels-photo-14534749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "name": "BurgerKing",
+        "rating": 4.0,
+        "city": "Midland",
+        "description": "Famous for Cheese Burgers and Baverages."
+    },
+    {
+        "imageUrl": "https://images.pexels.com/photos/9661469/pexels-photo-9661469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "name": "Five Guys",
+        "rating": 4.7,
+        "city": "Saginaw",
+        "description": "Famous for Oreo shakes and french fries."
+    },
+   {
+    "imageUrl": "https://images.pexels.com/photos/16037761/pexels-photo-16037761/free-photo-of-entrance-to-the-dining-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "name": "Wendy's",
+    "rating": 3.8,
+    "city": "Grand Rapids",
+    "description": "A vegan paradise for plant-based food lovers and chicken nuggets."
+}
+    ,
+    {
+    "imageUrl": "https://images.pexels.com/photos/7772238/pexels-photo-7772238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "name": "Chipotle",
+    "rating": 7.3,
+    "city": "Detroit",
+    "description": "The best Mexican food and Tacos."
+  },
+  {
+    "imageUrl": "https://images.pexels.com/photos/3714786/pexels-photo-3714786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "name": "McDonalds",
+    "rating": 4.1,
+    "city": "Mount Pleasant",
+    "description": "The world's largest and most popular fast-food brands, serving a variety of items such as hamburgers, fries, chicken products, breakfast items, and more."
+},
+{
+    "imageUrl": "https://images.pexels.com/photos/14534749/pexels-photo-14534749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "name": "BurgerKing",
+    "rating": 9.0,
+    "city": "Midland",
+    "description": "Famous for Cheese Burgers and Baverages."
+},
+{
+    "imageUrl": "https://images.pexels.com/photos/9661469/pexels-photo-9661469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "name": "Five Guys",
+    "rating": 4.7,
+    "city": "Saginaw",
+    "description": "Famous for Oreo shakes and french fries."
+},
+{
+"imageUrl": "https://images.pexels.com/photos/16037761/pexels-photo-16037761/free-photo-of-entrance-to-the-dining-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+"name": "Wendy's",
+"rating": 3.8,
+"city": "Grand Rapids",
+"description": "A vegan paradise for plant-based food lovers and chicken nuggets."
+}
+,
+{
+"imageUrl": "https://images.pexels.com/photos/7772238/pexels-photo-7772238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+"name": "Chipotle",
+"rating": 4.3,
+"city": "Detroit",
+"description": "The best Mexican food and Tacos."
+},
+{
+    "imageUrl": "https://images.pexels.com/photos/3714786/pexels-photo-3714786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "name": "McDonalds",
+    "rating": 4.1,
+    "city": "Mount Pleasant",
+    "description": "The world's largest and most popular fast-food brands, serving a variety of items such as hamburgers, fries, chicken products, breakfast items, and more."
+},
+{
+    "imageUrl": "https://images.pexels.com/photos/3714786/pexels-photo-3714786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "name": "McDonalds",
+    "rating": 4.1,
+    "city": "Mount Pleasant",
+    "description": "The world's largest and most popular fast-food brands, serving a variety of items such as hamburgers, fries, chicken products, breakfast items, and more."
+}
+  ];
+  
+  
