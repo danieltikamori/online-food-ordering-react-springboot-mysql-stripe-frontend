c6b86acd21e9186d7f2511d6282d0e5ad32e975c
danieltikamori <tikamori.itiro@gmail.com>
2024-08-12 20:53:03 +0900

    Add meal samples for Home

---
+++ src/component/Home/TopMeals.jsx
@@ -0,0 +1,35 @@
+export const topmeals = [
+    {
+        image: "https://images.pexels.com/photos/6072095/pexels-photo-6072095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title: "Pizza"
+    },
+    {
+        image: "https://images.pexels.com/photos/2983103/pexels-photo-2983103.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title: "Burger"
+    },
+    {
+        image: "https://images.pexels.com/photos/10218397/pexels-photo-10218397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title: "Sandwich"
+    },
+    {
+        image: "https://images.pexels.com/photos/7394819/pexels-photo-7394819.jpeg",
+        title: "Biryani"
+    },
+    {
+        image:"https://images.pexels.com/photos/2347311/pexels-photo-2347311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title:"Noodles"
+    },
+    {
+        image:"https://images.pexels.com/photos/1854652/pexels-photo-1854652.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title:"Cakes"
+
+    },
+    {
+        image:"https://images.pexels.com/photos/15924270/pexels-photo-15924270/free-photo-of-cups-with-cappuccinos-on-wooden-board-on-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title:"Baverages"
+    },
+    {
+        image:"https://images.pexels.com/photos/2087748/pexels-photo-2087748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title:"Tacos"
+    }
+];
